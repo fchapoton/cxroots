b32000edd814cc2aada8173f0d56ca5402196de7
Robert Parini <robert.parini@gmail.com>
2017-10-12 09:26:44 +0100

    Delete everything except tests

--- cxroots/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Contours import Circle, Annulus, AnnulusSector, Rectangle
-from .RootFinder import findRoots
-from .DemoRootFinder import demo_findRoots
-from .CountRoots import count_enclosed_roots
-from .CxDerivative import CxDeriv
--- docs_src/docex_contour.py
+++
@@ -1,8 +0,0 @@
-from numpy import pi
-
-from cxroots import Circle, Annulus, AnnulusSector, Rectangle
-
-Circle(0, 2).show('circle.png')
-Rectangle([-2,2],[-1,1]).show('rectangle.png')
-Annulus(0, [1,2]).show('annulus.png')
-AnnulusSector(0, [1,2], [0,pi]).show('annulussefig.png')
